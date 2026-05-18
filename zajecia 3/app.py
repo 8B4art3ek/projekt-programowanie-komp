3fc7379099f51eac71c37552625b21506277e78f
Bartłomiej Wysokiński <bartek.wysokinski.1ib@gmail.com>
2026-05-18 12:17:41 +0200

    poprzedni tydzien

--- zajecia 3/app.py
+++ zajecia 3/app.py
@@ -165,7 +165,43 @@ def api_tasks_add():
     return jsonify([dict(row)]), 201
 # curl -X POST http://127.0.0.1:5000/api/tasks -H "Content-Type: application/json" -d '{"title": "task"}'
 
+@app.route("/api/tasks/<int:task_id>", methods=["PUT", "PATCH"])
+def api_tasks_update(task_id):
+    db = get_db()
+    row = db.execute("SELECT id FROM tasks WHERE id = ?", [task_id]).fetchone()
+    if row is None:
+        abort(404, description="Task not found")
     
+    data = request.get_json()
+    if not data:
+        abort(400, description="Missing JSON")
+    title = data.get("title")
+    done = data.get("done")
+
+    if title:
+        title = title.strip()
+        if len(title) < 4:
+            abort(400, description="Title must have at least 4 chars")
+        existingTask = db.execute("SELECT id FROM tasks WHERE title LIKE ?", [title]).fetchone()
+        if existingTask:
+            abort(400, description="There already is a task with this title")
+        db.execute("UPDATE tasks SET title = ? WHERE id = ?", [title, task_id])
+    if done:
+        db.execute("UPDATE tasks SET done = ? WHERE id = ?", [done, task_id])
+    db.commit()
+    updated_row = db.execute("SELECT id, title, done, created_at FROM tasks WHERE id = ?", [task_id]).fetchone()
+    return jsonify(dict(updated_row))
+
+@app.route("/api/tasks/<int:task_id>", methods=["DELETE"])
+def api_tasks_delete(task_id):
+    db = get_db()
+    cur = db.execute("DELETE FROM tasks WHERE id = ?", [task_id])
+    db.commit()
+
+    if cur.rowcount == 0:
+        abort(404, description="Task not found")
+
+    return "", 204
 
 if __name__ == "__main__":
     app.run(debug=True)
